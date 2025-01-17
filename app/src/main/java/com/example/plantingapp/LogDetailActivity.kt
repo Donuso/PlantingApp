cb4f786b9d911d15de6d6d5cf645e8b899cd9198
caohuilong8 <3313355346@qq.com>
2025-01-17 12:19:06 +0800

    日志详情页
    1

--- app/src/main/java/com/example/plantingapp/LogDetailActivity.kt
+++ app/src/main/java/com/example/plantingapp/LogDetailActivity.kt
@@ -1,7 +1,10 @@
 package com.example.plantingapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LogDetailActivity : AppCompatActivity() {
@@ -9,8 +12,44 @@ class LogDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_detail)
 
-        val logDetail = findViewById<TextView>(R.id.log_detail)
-        val logText = intent.getStringExtra("LOG_TEXT")
-        logDetail.text = logText
+        // 获取传递过来的数据
+        val plantName = intent.getStringExtra("PLANT_NAME")
+        val date = intent.getStringExtra("DATE")
+
+        // 设置植物名称和日期
+        val plantNameTextView = findViewById<TextView>(R.id.plant_name)
+        plantNameTextView.setText(plantName)
+        val dateTextView = findViewById<TextView>(R.id.date)
+        dateTextView.setText(date)
+
+        // 设置编辑日期的按钮点击事件
+        val editDateButton = findViewById<Button>(R.id.datechange_button)
+        editDateButton.setOnClickListener {
+            // 打开日期选择界面
+            val intent = Intent(this, DateSelectionActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 设置回到今天的按钮点击事件
+        //val backToTodayButton = findViewById<Button>(R.id.back_to_today_button)
+        //backToTodayButton.setOnClickListener {
+          //  finish() // 返回到上一个活动
+        //}
+
+        // 设置增加标签的按钮点击事件
+        //val tagsTextView = findViewById<TextView>(R.id.tags)
+        //tagsTextView.setOnClickListener {
+            // 打开标签选择界面
+            //val intent = Intent(this, TagSelectionActivity::class.java)
+            //startActivity(intent)
+        //}
+
+        // 设置增加图片的按钮点击事件
+        val addImageButton = findViewById<Button>(R.id.add_image)
+        addImageButton.setOnClickListener {
+            // 打开图片选择界面
+            val intent = Intent(this, ImageSelectionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
