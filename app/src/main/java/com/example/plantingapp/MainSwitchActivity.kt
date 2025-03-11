77ed1b5f392d165daa61f3be1327e4985da2e2bd
Donuso <wzc3318034587@outlook.com>
2025-03-12 01:52:57 +0800

    运行测试代码补全

--- app/src/main/java/com/example/plantingapp/MainSwitchActivity.kt
+++ app/src/main/java/com/example/plantingapp/MainSwitchActivity.kt
@@ -1,24 +1,58 @@
 package com.example.plantingapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.plantingapp.fragments.LogFragment
+import com.example.plantingapp.fragments.MainFragment
+import com.example.plantingapp.fragments.MeFragment
+import com.example.plantingapp.fragments.TodoFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 /*
 * 这是fragment的框架显示页
 * */
 
 class MainSwitchActivity : AppCompatActivity() {
+
+    private val fragmentManager: FragmentManager by lazy { supportFragmentManager }
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
         setContentView(R.layout.activity_main_switch)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        setBottomNavigationListener()
+    }
+
+    private fun setBottomNavigationListener() {
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_main -> {
+                    switchFragment(MainFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navigation_logs -> {
+                    switchFragment(LogFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navigation_todos -> {
+                    switchFragment(TodoFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navigation_me -> {
+                    switchFragment(MeFragment())
+                    return@setOnItemSelectedListener true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun switchFragment(fragment: Fragment) {
+        fragmentManager.beginTransaction()
+            .replace(R.id.frameLayoutContent, fragment)
+            .commit()
     }
 }
