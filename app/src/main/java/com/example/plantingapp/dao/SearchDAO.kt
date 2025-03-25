bda76a6d27850d9dd2a4103c07331c29838a5594
AngusZHJ <2844548910@qq.com>
2025-03-25 08:51:28 +0800

    Create SearchDAO.kt

---
+++ app/src/main/java/com/example/plantingapp/dao/SearchDAO.kt
@@ -0,0 +1,9 @@
+package com.example.plantingapp.dao
+
+import android.content.Context
+import com.example.plantingapp.DBHelper
+
+class SearchDAO(context: Context) {
+    private val dbHelper = DBHelper(context)
+    
+}
